8d88c35935e569584f9dfab6832a7e6a40d06a61
ian <iansmithxv@gmail.com>
2024-04-29 04:41:54 +0300

    User model & Auth controllers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("Hello Node");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("Server running on http://localhost:8080/");
+});
+
+const MONGO_URL =
+  "mongodb+srv://iansmith:40password@cluster0.atrx5m0.mongodb.net/";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
